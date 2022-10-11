3d48dfda2a0e2cfc44834d6bb4948d4c842015f0
Mariya <masa995@yandex.ru>
2022-10-11 05:27:24 +0300

    Add adaptive for app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,18 @@ import { NavLink } from "react-router-dom";
 
 import { FavoritesLink } from "../index";
 
+import logo from "../../static/img/droid.png";
 import style from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={style.header__container}>
       <div className={`${style.header__inner} container`}>
+        <img
+          className={style.logo}
+          src={logo}
+          alt="Logo"
+        />
         <ul className={style.header__list}>
           <li className={style.header__item}>
             <NavLink end to={"/"}>Home</NavLink>
